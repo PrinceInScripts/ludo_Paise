a8261ef8740ff5aa5f8b97f03ab84730b4c2c17e
Jaykishan Rawat <jksonu1436@gmail.com>
2024-09-25 00:07:35 +0530

    'done'

--- user-app/db.php
+++ user-app/db.php
@@ -1,6 +1,6 @@
 <?php 
 session_start();
-$con = mysqli_connect('localhost','root','','matkaplay');
+$con = mysqli_connect('localhost','root','','ludopaisa');
 if(!$con){
     echo "Database connection failed...";
 }
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '845f91fb359b0d17c0da6005910355c805e63c85',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '845f91fb359b0d17c0da6005910355c805e63c85',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
