a1dc66fa192d789b72400c47ec92a766968021a2
princekumar <princepk7878kumar@gmail.com>
2024-09-30 14:09:50 +0530

    done

--- admin/addUser.php
+++ admin/addUser.php
@@ -1,74 +1,53 @@
 <?php
-include ("db.php");
-include ("top.php");
+include("db.php");
+include("top.php");
 
 
+
+if (isset($_POST['submit'])) {
+    $mobile = trim($_POST['mobile']);
+
+    if (empty($mobile)) {
+        $error = "Mobile number is required.";
+    } else {
+       
+        $mobile = mysqli_real_escape_string($con, $mobile);
+        $sql = "INSERT INTO users (mobile) VALUES ('$mobile')";
+        $res=mysqli_query($con,$sql);
+
+       
+        header("location:allUsers.php");
+         
+
+         
+    }
+}
 ?>
 
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
+<div class="content-wrapper">
     <div class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1 class="m-0">Add User</h1>
-          </div>
-          <div class="col-sm-6">
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="col-md-6">
-            <!-- general form elements -->
-            <div class="card card-primary">
-              <div class="card-header">
-                <h3 class="card-title">Quick Example</h3>
-              </div>
-              <!-- /.card-header -->
-              <!-- form start -->
-              <form>
-                <div class="card-body">
-                  <div class="form-group">
-                    <label for="exampleInputEmail1">Email address</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
-                  </div>
-                  <div class="form-group">
-                    <label for="exampleInputPassword1">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-                  </div>
-                  <div class="form-group">
-                    <label for="exampleInputFile">File input</label>
-                    <div class="input-group">
-                      <div class="custom-file">
-                        <input type="file" class="custom-file-input" id="exampleInputFile">
-                        <label class="custom-file-label" for="exampleInputFile">Choose file</label>
-                      </div>
-                      <div class="input-group-append">
-                        <span class="input-group-text">Upload</span>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="form-check">
-                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                  </div>
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0">Add User</h1>
                 </div>
-                <!-- /.card-body -->
-
-                <div class="card-footer">
-                  <button type="submit" class="btn btn-primary">Submit</button>
-                </div>
-              </form>
             </div>
-            <!-- /.card -->
+        </div>
 
-          
-          </div>
-   
-   
+        <form id="quickForm" method="POST">
+            <div class="card-body">
+                <div class="form-group">
+                    <label for="exampleInputMobile">Enter User Mobile Number</label>
+                    <input type="text" name="mobile" class="form-control" id="exampleInputMobile" placeholder="Enter Mobile Number">
+                </div>
+            </div>
+            <div class="card-footer">
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+        </form>
+    </div>
+</div>
 
 <?php
-include ("footer.php");
+include("footer.php");
 ?>
