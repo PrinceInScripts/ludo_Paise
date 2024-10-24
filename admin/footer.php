f228583c1931b91416f3bf6638123bb23f28a946
Jaykishan Rawat <jksonu1436@gmail.com>
2024-10-25 00:03:48 +0530

    DSDS

--- admin/footer.php
+++ admin/footer.php
@@ -1,12 +1,4 @@
 
-  <!-- /.content-wrapper -->
-  <footer class="main-footer">
-    <strong>Copyright &copy; 2024 <a href="https://ludopaisa.com">ludopaisa.com</a>.</strong>
-    All rights reserved.
-    <div class="float-right d-none d-sm-inline-block">
-      <b>Version</b> 3.2.0
-    </div>
-  </footer>
 
   <!-- Control Sidebar -->
   <aside class="control-sidebar control-sidebar-dark">
